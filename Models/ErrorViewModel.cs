a07c3cd06131989231b9db9a76a7ca182c3f973b
Chris <chris_oleary@hotmail.co.uk>
2020-08-19 10:26:39 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMAS_Client_Portal2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
